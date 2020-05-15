aec9ebbff458c908d0b52d665acca402700d4005
teachmemicro <kurimawxx00@gmail.com>
2020-05-15 20:32:46 +0800

    Changed to AJAX for auto-update

--- tempestv2/tempestv2.ino
+++ tempestv2/tempestv2.ino
@@ -1,7 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
-
 #include "DHT.h"
 
 #define DHTPIN 2
@@ -18,6 +17,7 @@ const char* password = "stacey0921";
 ESP8266WebServer server(80);   //instantiate server at port 80 (http port)
 
 String page = "";
+String text = "";
 float temp, hum; 
 void setup(void){
 
@@ -38,8 +38,32 @@ void setup(void){
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+  server.on("/data.txt", [](){
+   text = (String)temp + "&" + (String)hum;
+   server.send(200, "text/html", text);
+ });
   server.on("/", [](){
-    page = "<h1>Tempest Web Server</h1><h3>Temperature:</h3> <h4>"+String(temp)+"</h4><h3>Humidity:</h3> <h4>"+String(hum)+"</h4>";
+    page = "<h1>Tempest V2</h1><div id=\"data\">""</div>\r\n";
+   page += "<script>\r\n";
+   page += "var x = setInterval(function() {loadData(\"data.txt\",updateData)}, 1000);\r\n";
+   page += "function loadData(url, callback){\r\n";
+   page += "var xhttp = new XMLHttpRequest();\r\n";
+   page += "xhttp.onreadystatechange = function(){\r\n";
+   page += " if(this.readyState == 4 && this.status == 200){\r\n";
+   page += " callback.apply(xhttp);\r\n";
+   page += " }\r\n";
+   page += "};\r\n";
+   page += "xhttp.open(\"GET\", url, true);\r\n";
+   page += "xhttp.send();\r\n";
+   page += "}\r\n";
+   page += "function updateData(){\r\n";
+   page += "var data = this.responseText;\r\n";
+   page += "var s = data.split('&')\r\n;";
+   page += "var temp = s[0];\r\n";
+   page += "var hum = s[1];\r\n";
+   page += " document.getElementById(\"data\").innerHTML = '<h3>Temperature: '+temp+' &#176;C</h3><h3>Humidity: '+hum+' %</h3>';\r\n";
+   page += "}\r\n";
+   page += "</script>\r\n";
     server.send(200, "text/html", page);
   });
   
